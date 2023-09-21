9298aca10147779fb99db309ab85d02bf1bbb99d
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-09-21 20:50:04 +0530

    basic UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Random from './components/Random'
+import Tag from './components/Tag'
 
 function App() {
   return (
-    <div>
-      hello
+    <div className='w-full h-screen flex flex-col background items-center overflow-hidden relative'>
+    <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 text-4xl font-bold'>Random Gifs</h1>
+    <div className='flex flex-col'>
+      <Random/>
+      <Tag/>
+    </div>
     </div>
   )
 }
---
+++ src/components/Random.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Random() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Random
